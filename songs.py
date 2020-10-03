8fc9e94e516a9b41fb09f96d073c44d5e220b26f
shreyashack <shreyasmohite11123@gmail.com>
2020-10-03 13:42:06 +0530

    second commit

--- songs.py
+++ songs.py
@@ -143,6 +143,9 @@ class Songs:
                             for name in names:
                                 f.write("\n")
                                 f.write(name.get("href"))
+                            
+
+                            '''
 
                         if website.get()=="ncs/15":
                             website_url.set("https://instrumentalfx.co/download-top-15-best-no-copyright-songs-ncs-2017/")
@@ -150,9 +153,10 @@ class Songs:
                             Soup=BeautifulSoup(response.text,"html.parser")
                             gather=Soup.findAll("a")
                             names=set(gather)
-                            for name in naems:
+                            for name in names:
                                 f.write("\n")
                                 f.write(name.get("href"))
+                                '''
 
                         if website.get()=="filmisongs/english":
                             website_url.set("https://filmisongs.com/english-mp3-songs-download/page/1/")
@@ -173,11 +177,12 @@ class Songs:
                                 f.write("\n")
                                 f.write(name.get("href"))
 
-                        if website.get()=="any":                            
+                        if website.get()=="Any":                            
                             response = requests.get(website_url.get())
                             Soup=BeautifulSoup(response.text,"html.parser")
                             gather=Soup.findAll("a")
-                            for name in gather:
+                            names=set(gather)
+                            for name in names:
                                 f.write("\n")
                                 f.write(name.get("href"))
 
@@ -191,9 +196,36 @@ class Songs:
                             for name in names:
                                 f.write("\n")
                                 f.write(name.get("href"))
+                            
+
+                        if website.get()=="songsmp3/yoyohoneysingh":                                                        
+                            website_url.set("https://www.songsmp3.biz/singers/yo-yo-honey-singh/page-1.html")
+                            response = requests.get(website_url.get())
+                            Soup=BeautifulSoup(response.text,"html.parser")
+                            gather=Soup.findAll("a",class_="dowbut")
+                            names=set(gather)
+                            for name in names:
+                                f.write("\n")
+                                f.write("https://www.songsmp3.biz{}".format(name.get("href")))
+
+
+                        if website.get()=="songsmp3/download":                                                        
+                            #website_url.set("https://www.songsmp3.biz/singers/yo-yo-honey-singh/page-1.html")
+                            response = requests.get(website_url.get())
+                            Soup=BeautifulSoup(response.text,"html.parser")
+                            gather=Soup.findAll("a",class_="dowbutzip")
+                            names=set(gather)
+                            for name in names:
+                                f.write("\n")
+                                f.write(name.get("href"))
 
 
                         
+                            
+
+
+
+                        #https://www.songsmp3.biz/category/top?type=today
 
                         
 
@@ -220,6 +252,82 @@ class Songs:
                         f.write(r.content)
             except:
                 pass
+
+        
+        def websites():
+            root1=Toplevel(self.root)
+            root1.title("Websites")
+            root1.geometry("400x400")
+            root1.resizable(0,0)
+
+            frame_web=Frame(root1,width=400,height=400,relief="ridge",bd=4)
+            frame_web.place(x=0,y=0)
+
+            mess_1=Label(frame_web,text="step 1:talk2trend/bollywood and talk2trend/english",anchor="w",font=('times new roman',11,'bold'))
+            mess_1.place(x=10,y=25)
+
+            mess_2=Label(frame_web,text="step 2: Click the scrape button to scrape the website",anchor="w",font=('times new roman',11,'bold'))
+            mess_2.place(x=10,y=45)
+
+            root1.mainloop()
+
+
+        def usermanual():
+
+            root2=Toplevel(self.root)
+            root2.title("User Manual")
+            root2.geometry("400x400")
+            root2.resizable(0,0)
+
+            frame_um=Frame(root2,width=400,height=400,relief="ridge",bd=4)
+            frame_um.place(x=0,y=0)
+
+            lab_heading=Label(frame_um,text="User Manual",font=('times new roman',13,'bold'))
+            lab_heading.place(x=130,y=0)
+
+            mess_1=Label(frame_um,text="step 1: Choose a website from the given combination box",anchor="w",font=('times new roman',11,'bold'))
+            mess_1.place(x=10,y=25)
+
+            mess_2=Label(frame_um,text="step 2: Click the scrape button to scrape the website",anchor="w",font=('times new roman',11,'bold'))
+            mess_2.place(x=10,y=45)
+
+            mess_3=Label(frame_um,text="step 3: Select the url of the given song",anchor="w",font=('times new roman',11,'bold'))
+            mess_3.place(x=10,y=65)
+
+            mess_4=Label(frame_um,text="step 4: Go to download songs tab ",anchor="w",font=('times new roman',11,'bold'))
+            mess_4.place(x=10,y=85)
+
+            mess_5=Label(frame_um,text="step 5: Place the song url in box  ",anchor="w",font=('times new roman',11,'bold'))
+            mess_5.place(x=10,y=105)
+
+            mess_6=Label(frame_um,text="step 6: Give the name to save the song name  ",anchor="w",font=('times new roman',11,'bold'))
+            mess_6.place(x=10,y=125)
+
+            mess_7=Label(frame_um,text="step 7: Click on download button  ",anchor="w",font=('times new roman',11,'bold'))
+            mess_7.place(x=10,y=145)
+
+            mess_8=Label(frame_um,text="step 8: Wait till the songs download  ",anchor="w",font=('times new roman',11,'bold'))
+            mess_8.place(x=10,y=165)
+
+            mess_9=Label(frame_um,text="step 9: Songs will be store automatically in folder  ",anchor="w",font=('times new roman',11,'bold'))
+            mess_9.place(x=10,y=185)
+
+            mess_10=Label(frame_um,text="step 10:Play the song and enjoy  ",anchor="w",font=('times new roman',11,'bold'))
+            mess_10.place(x=10,y=205)
+
+            root2.mainloop()
+
+
+        def Sourcecode():
+            root3=Toplevel(self.root)
+            root3.title("Source Code")
+            root3.geometry("400x400")
+            root3.resizable(0,0)
+
+            frame_sc=Frame(root3,width=400,height=400,relief="ridge",bd=4)
+            frame_sc.place(x=0,y=0)
+
+            root3.mainloop()
 
                             
 
@@ -264,30 +372,30 @@ class Songs:
         messages2.place(x=229,y=4)
 
 
-        messages3=Message(about,text="This project is all about fun and learning this project will help you to find out how you can scrape songs in different all this project goes to the author of this project \
-            shreyas mohite",font='Arial 12', anchor=W)
+        messages3=Message(about,text="The Source code of this project is available on github  you can use it as per your requirement this will help you out.The given buttons will show you how to use this software \
+            ",font='Arial 12', anchor=W)
         messages3.place(x=454,y=4)
 
 
         #==================button on About nootbook=============================================#  
 
-        but_user_manual=Button(about,text="User Manual",font=('times new roman',12,"bold"),width=15,cursor="hand2")
+        but_user_manual=Button(about,text="User Manual",font=('times new roman',12,"bold"),width=15,cursor="hand2",command=usermanual)
         but_user_manual.place(x=35,y=170)
         but_user_manual.bind("<Enter>",on_enter1)
         but_user_manual.bind("<Leave>",on_leave1)
 
-        but_websites=Button(about,text="Websites",font=('times new roman',12,"bold"),width=15,cursor="hand2")
+        but_websites=Button(about,text="Websites",font=('times new roman',12,"bold"),width=15,cursor="hand2",command=websites)
         but_websites.place(x=260,y=170)
         but_websites.bind("<Enter>",on_enter2)
         but_websites.bind("<Leave>",on_leave2)
 
-        but_Source=Button(about,text="Source code",font=('times new roman',12,"bold"),width=15,cursor="hand2")
+        but_Source=Button(about,text="Source code",font=('times new roman',12,"bold"),width=15,cursor="hand2",command=Sourcecode)
         but_Source.place(x=490,y=170)
         but_Source.bind("<Enter>",on_enter3)
         but_Source.bind("<Leave>",on_leave3)
 
 
-        
+
 
     #=============================Download songs tab=============================================================#
 
@@ -295,7 +403,7 @@ class Songs:
 
         lab_url=Label(download_songs,text="Enter Song Url",font=('times new roman',12,'bold'),background="grey87")
         lab_url.place(x=40,y=30)
-
+    
         Ent_songs_url=Entry(download_songs,textvariable=song_url,width=50,font=('times new roman',12,'bold'),bd=4,relief='ridge')
         Ent_songs_url.place(x=230,y=30)
 
@@ -304,10 +412,6 @@ class Songs:
 
         Ent_save_as_name=Entry(download_songs,textvariable=song_name,width=20,font=('times new roman',12,'bold'),bd=4,relief='ridge')
         Ent_save_as_name.place(x=230,y=105)
-
-
-
-
 
 
         but_download_songs=Button(download_songs,text="Download",font=('times new roman',12,"bold"),width=15,cursor="hand2",command=download)
@@ -328,7 +432,7 @@ class Songs:
         lab_web.place(x=20,y=10)
         
 
-        list_websites=["any","random/filmisongs/cdn","talk2trend/bollywood","talk2trend/english","ncs/15","filmisongs/bollywood","filmisongs/english"]
+        list_websites=["Any","random/filmisongs/cdn","talk2trend/bollywood","talk2trend/english","ncs/15","filmisongs/bollywood","filmisongs/english","songsmp3/yoyohoneysingh","songsmp3/download"]
         list_websites_combo=Combobox(scrape_songs,values=list_websites,font=('arial',10),width=19,state="readonly",textvariable=website)
         list_websites_combo.set("select websites")
         list_websites_combo.place(x=180,y=10)
@@ -369,25 +473,9 @@ class Songs:
         prg=Progressbar(thirdframe,length=688,orient=HORIZONTAL,mode='indeterminate')
         prg.place(x=0,y=0)
         
-        
-
-
-
 
 
 if __name__ == "__main__":
     root=Tk()
     app=Songs(root)
     root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-    
